7193c563c643a78d757384cffe48ab3a04895f31
Bill Keese <bill@dojotoolkit.org>
2020-02-15 07:44:54 +0900

    Refactor metrics.js to not use the domReady! plugin. It doesn't play nice with Webpack.

--- metrics.js
+++ metrics.js
@@ -1,18 +1,24 @@
-define(["requirejs-domready/domReady!"], function () {
-	// This is the getScroll() method from dojox/html/metrics.js
-
-	var n = document.createElement("div");
-	n.style.cssText = "top:0;left:0;width:100px;height:100px;overflow:scroll;position:absolute;visibility:hidden;";
-	document.body.appendChild(n);
-
-	var scroll = {
-		w: n.offsetWidth - n.clientWidth,
-		h: n.offsetHeight - n.clientHeight
-	};
-
-	document.body.removeChild(n);
+define([], function () {
+	var scroll;
 
 	return {
-		getScrollbar: function () { return scroll; }
+		getScrollbar: function () {
+			if (!scroll) {
+				// This is the getScroll() method from dojox/html/metrics.js
+				var n = document.createElement("div");
+				n.style.cssText =
+					"top:0;left:0;width:100px;height:100px;overflow:scroll;position:absolute;visibility:hidden;";
+				document.body.appendChild(n);
+
+				scroll = {
+					w: n.offsetWidth - n.clientWidth,
+					h: n.offsetHeight - n.clientHeight
+				};
+
+				document.body.removeChild(n);
+			}
+
+			return scroll;
+		}
 	};
 });
